b2d3eb0f4e991617c8c44b8015d1920b7325442e
Anas <anasrajput99112@gmail.com>
2026-02-09 18:20:11 +0500

    complete

--- js/main.js
+++ js/main.js
@@ -5,4 +5,22 @@ menubar.addEventListener("click", () => {
   navlink.classList.toggle("nav-active");
 });
 
-   var swiper = new Swiper(".mySwiper", {});
+
+
+// swiper js 
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 24,
+  slidesPerView: 1.1,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
